4caf6a1d564428f2907ce1c73773f0e12c673323
EcharpeBleue <zawiczrczarny@gmail.com>
2023-10-23 15:19:50 +0200

    Update Exercice4.js

--- Exercice4.js
+++ Exercice4.js
@@ -1,7 +1,10 @@
 let texte = ";"
 for (let i=0;i<17;i++)
+//let i = 0;
+//while (i<=16)
 {
     texte = texte+(Math.pow(2,i));
+//  texte += `2^${i}=${Math.pow(2,i)}`;
     let separateur = " - "
     texte = texte+separateur
 }
