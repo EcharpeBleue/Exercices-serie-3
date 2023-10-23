0449d21a41a21ac540ba00e3273dfe640bbbac84
EcharpeBleue <zawiczrczarny@gmail.com>
2023-10-23 13:31:02 +0200

    Update Exercice2.js

--- Exercice2.js
+++ Exercice2.js
@@ -4,3 +4,11 @@ for (let i=10;i>=1;i--)
     texte = texte+i;
 }
 console.log(texte);
+
+// let texte = ""
+// for (let i=0;i<=9;i++)
+// {
+// let resultat = 10-i
+// texte+=resultat;
+// }
+// console.log(texte);
